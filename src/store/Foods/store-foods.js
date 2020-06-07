0126e8a45419ee63726b9a498ce9d54910f590ec
Larry Kagan <larry@superiocity.com>
2020-06-07 12:21:41 -0400

    Delete a food

--- src/store/Foods/store-foods.js
+++ src/store/Foods/store-foods.js
@@ -1,36 +1,51 @@
-export default {
-  state: {
-    foods: [
-      {
-        id: 1,
-        name: "Burger",
-        description:
-          "A burger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
-        imageUrl: "https://i.imgur.com/0umadnY.jpg",
-        rating: 4
-      },
-      {
-        id: 2,
-        name: "Pizza",
-        description:
-          "Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough.",
-        imageUrl: "https://i.imgur.com/b9zDbyb.jpg",
-        rating: 5
-      },
-      {
-        id: 3,
-        name: "Sprouts",
-        description:
-          "The Brussels sprout is a member of the Gemmifera Group of cabbages, grown for its edible buds.",
-        imageUrl: "https://i.imgur.com/RbKjUjB.jpg",
-        rating: 1
-      }
-    ]
-  },
+import Vuex from 'vuex';
 
-  getters: {
-    foods(state) {
-      return state.foods;
+export default {
+    state: {
+        foods: [
+            {
+                id: 1,
+                name: "Burger",
+                description:
+                    "A burger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
+                imageUrl: "https://i.imgur.com/0umadnY.jpg",
+                rating: 4
+            },
+            {
+                id: 2,
+                name: "Pizza",
+                description:
+                    "Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough.",
+                imageUrl: "https://i.imgur.com/b9zDbyb.jpg",
+                rating: 5
+            },
+            {
+                id: 3,
+                name: "Sprouts",
+                description:
+                    "The Brussels sprout is a member of the Gemmifera Group of cabbages, grown for its edible buds.",
+                imageUrl: "https://i.imgur.com/RbKjUjB.jpg",
+                rating: 1
+            }
+        ]
+    },
+
+    getters: {
+        foods(state) {
+            return state.foods;
+        }
+    },
+
+    mutations: {
+        deleteFood(state, id) {
+            const index = state.foods.findIndex(food => food.id === id);
+            state.foods.splice(index, 1);
+        }
+    },
+
+    actions: {
+        deleteFood({commit}, id) {
+            commit("deleteFood", id);
+        }
     }
-  }
 };
